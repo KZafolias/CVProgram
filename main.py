6904f609590fc906fe52c346acb42e34ccf2bcc7
Kostantinos Zafolias <zafoliaskostas@gmail.com>
2021-10-19 23:30:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+print('''
+    Hello!
+    Welcome to my CV program!
+        you can type things such as name, experience, education etc.
+''')
+user_input =input("So what you want to know")
+
+if user_input == "name ":
+    print("Kostantnos Zafolias")
+
+elif user_input == "Contact information":
+    print("E-Mail: zafoliaskostas@gmail.com")
+
+elif user_input == "Personal Profile":
+    print("https://github.com/KZafolias")
+
+elif user_input == "Work Experience":
+    print("https://kzafolias.github.io/")
+
+elif user_input == "Education":
+    print('''
+        44 Elementary School 2015-2019
+        44 Middle School 2020-2021
+        65 High School 2021-present
+    ''')
+elif user_input == "Skills":
+    print("Programming, Data Miner, Financing, Economic Analysis")
